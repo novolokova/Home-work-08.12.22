da55474290e75995a135753cc3656527ddcffa00
Margarita <m.novolokova@gmail.com>
2022-12-08 17:35:42 +0200

    Add task

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-'use strict'
+"use strict";
 
+const user = {
+  id: 1,
+  privateInfo: {
+    fname: "Brad",
+    sname: "Pitt",
+    bday: {
+      day: 18,
+      month: 12,
+      year: 1963,
+    },
+    children: ["Helen", "Alex", "Georg", "Anna"],
+  },
+  contactInfo: {
+    phone: {
+      work: "123-12-45",
+      mobile: "005-002-003",
+    },
+    adress: {
+      town: "ZP",
+      street: "12Avenu",
+      house: 45,
+    },
+    mail: "brad@gmail.com",
+  },
+  profession: "actor",
+};
 
+// Створити змінні для року народження, імені другої дитини, робочого телефону.
+// Бажано одинм рядком, використовуючи деструктуризацію.
 
+const {
+  privateInfo: {
+    bday: { year },
+  },
+  privateInfo: {
+    children: [, two],
+  },
+  contactInfo: {
+    phone: { work },
+  },
+} = user;
 
-
-
-
+console.log(year, two, work);
